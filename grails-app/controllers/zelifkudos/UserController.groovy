247c5a357af56ff1c6436f36df3b74a25c5fa7fa
Seah <seah@zelifcam.net>
2026-05-01 11:08:37 -0500

    sort only show activate users on the main user list.

--- grails-app/controllers/zelifkudos/UserController.groovy
+++ grails-app/controllers/zelifkudos/UserController.groovy
@@ -12,7 +12,7 @@ class UserController {
         User currentUser = request.currentUser
         Map<Long, Integer> kudosCounts = kudosService.countKudosForAllUsers()
         Map<Long, Integer> sentCounts = kudosService.countSentForAllUsers()
-        List<User> users = User.list().sort { -(sentCounts[it.id] ?: 0) }
+        List<User> users = User.findAllByActivated(true).sort { -(sentCounts[it.id] ?: 0) }
         int myKudosCount = kudosCounts[currentUser.id] ?: 0
         List<Kudos> recentMessages = kudosService.getRecentKudosForUser(currentUser.id, 3).findAll { it.message }
         Map<Long, String> feelings = feelingService.getAllFeelings()
